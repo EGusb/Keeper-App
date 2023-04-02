9da9ed1071fa5b6356090163c70f26755d8691d0
Emiliano Gusberti <emggw@hotmail.com>
2023-04-02 01:00:11 -0300

    Add 3 notes with attrs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,9 @@ function App() {
   return (
     <div>
       <Header />
-      <Note />
+      <Note title="First Note" content="This was the first Note ever!"/>
+      <Note title="Second Note" content="Now there's two of them!"/>
+      <Note title="Note #3" content="Third time's the charm!"/>
       <Footer />
     </div>
   );
