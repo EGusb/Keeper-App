c7849f6b733cfc66dc207a0787a3b2cf5f5cc194
Emiliano Gusberti <emggw@hotmail.com>
2023-04-03 23:55:08 -0300

    Format for arrow fn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,9 +10,9 @@ function App() {
   return (
     <div>
       <Header />
-      {notes.map((note) => {
-        return <Note key={note.key} title={note.title} content={note.content}/>;
-      })}
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
