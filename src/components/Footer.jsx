33a6a9225aa7baba0a001cdd7254f99be22c9a5d
Emiliano Gusberti <emggw@hotmail.com>
2023-04-12 22:27:42 -0300

    Remove name var from inside function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
+const name = "Emiliano Gusberti";
+
 function Footer() {
-  const name = "Emiliano Gusberti";
   const currentYear = new Date().getFullYear();
 
   return (
