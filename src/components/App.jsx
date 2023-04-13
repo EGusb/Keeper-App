fa70a07fb316d567f9adc561f186529e4335b157
Emiliano Gusberti <emggw@hotmail.com>
2023-04-12 23:00:49 -0300

    Order imports alphabetically, change styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
 import CreateArea from "./CreateArea";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+import React from "react";
 
 function App() {
   return (
