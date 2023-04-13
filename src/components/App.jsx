62a13b45565240e77ca2d0ab9c5989def872a79f
Emiliano Gusberti <emggw@hotmail.com>
2023-04-12 22:41:20 -0300

    Remove notes array reference

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-
-import Footer from "./Footer";
 import Header from "./Header";
+import Footer from "./Footer";
 import Note from "./Note";
-
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map((note, index) => (
-        <Note key={index} title={note.title} content={note.content} />
-      ))}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
