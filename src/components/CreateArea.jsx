95ab88c3114ccd9daa9fa7a405988c5bfbc767a9
Emiliano Gusberti <emggw@hotmail.com>
2023-04-13 00:17:35 -0300

    Add isExpanded var & animation when clicked

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import AddTaskIcon from "@mui/icons-material/AddTask";
-import Fab from '@mui/material/Fab';
+import Fab from "@mui/material/Fab";
+import { Zoom } from "@mui/material";
 
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
   const [note, setNote] = useState({ title: "", content: "" });
 
+  function expand() {
+    setExpanded(true);
+  }
+  
   function addNote(event) {
     if (note.title !== "" || note.content !== "") {
       props.onAdd(note);
@@ -28,23 +34,28 @@ function CreateArea(props) {
 
   return (
     <div>
-	<form className="create-note">
-        <input
-          name="title"
-          placeholder="Title"
-          onChange={handleChange}
-          value={note.title}
-        />
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            placeholder="Title"
+            onChange={handleChange}
+            value={note.title}
+          />
+        )}
         <textarea
           name="content"
           placeholder="Write a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
           onChange={handleChange}
           value={note.content}
+          onClick={expand}
         />
-        <Fab onClick={addNote}>
-          <AddTaskIcon />
-        </Fab>
+        <Zoom in={isExpanded}>
+          <Fab onClick={addNote}>
+            <AddTaskIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
