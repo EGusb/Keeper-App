8e443d3a9060f349ba7e5075fd99a5c5ccc39415
Emiliano Gusberti <emggw@hotmail.com>
2023-04-12 23:35:08 -0300

    Add onDelete function, inline function for btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,7 @@ import Note from "./Note";
 import React, { useState } from "react";
 
 function App() {
-  const [notes, setNotes] = useState([
-    { title: "Note title", content: "Note content" },
-  ]);
+  const [notes, setNotes] = useState([]);
 
   function addNote(note) {
     setNotes((prevNotes) => {
@@ -15,12 +13,28 @@ function App() {
     });
   }
 
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea onAdd={addNote}/>
+      <CreateArea onAdd={addNote} />
       {notes.map((noteItem, index) => {
-        return <Note key={index} title={noteItem.title} content={noteItem.content} />;
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
       <Footer />
     </div>
